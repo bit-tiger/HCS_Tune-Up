7def9390c2d92e5580c1b7698ec59602412fbdd4
bit-tiger <76663384+bit-tiger@users.noreply.github.com>
2021-10-14 16:46:09 -0700

    added main .bat file

---
+++ Tune-Up V-0.1.0.bat
@@ -0,0 +1,17 @@
+@echo off
+
+:: **************************************
+: Menu_p-1
+	
+	echo. HCS Tune-Up Script V0.1.0
+	echo.   (Script by bit-tiger)
+	echo. 
+	echo. h - help         i - info
+	echo. *************************
+	echo. 1. Run CCleaner
+	echo. 2. Run MalwareBytes
+	echo. 3. Install tools and make HCS-Tools Folder
+	echo. 4. Update tools 
+	echo. 
+	
+:: **************************************
