c4adce04f72abe5205ca71e4070788341d3b7c1a
bit-tiger <76663384+bit-tiger@users.noreply.github.com>
2021-10-16 00:47:29 -0700

    got menu functions working

--- Tune-Up V-0.1.0.bat
+++ Tune-Up V-0.1.0.bat
@@ -1,17 +1,43 @@
 @echo off
+color 0a
+title HCS Tune-Up Script V0.1.0
+goto :menu
+
 
 :: **************************************
-: Menu_p-1
+:: the menu interface 
+: menu
+cls
 	
-	echo. HCS Tune-Up Script V0.1.0
-	echo.   (Script by bit-tiger)
+	echo.         HCS Tune-Up Script V0.1.0
+	echo.           (Script by bit-tiger)
 	echo. 
-	echo. h - help         i - info
-	echo. *************************
+	echo. h - help                          i - info
+	echo. ******************************************
 	echo. 1. Run CCleaner
 	echo. 2. Run MalwareBytes
 	echo. 3. Install tools and make HCS-Tools Folder
-	echo. 4. Update tools 
-	echo. 
+	echo. 4. Update tools  
+	
+	:: **************************************
+	:: menu logic
+	
+	choice /c hi1234 /n
+	cls
+	IF %errorlevel%==1 (
+		goto :help
+	) ELSE IF %errorlevel%==2 (
+		goto :info
+	) ELSE IF %errorlevel%==3 (
+		goto :ccleaner
+	) ELSE IF %errorlevel%==4 (
+		goto :malwarebytes
+	) ELSE IF %errorlevel%==5 (
+		goto :install
+	) ELSE IF %errorlevel%==6 (
+		goto :update
+	) ELSE echo. please pick an option
+	pause
+	goto :menu
 	
 :: **************************************
